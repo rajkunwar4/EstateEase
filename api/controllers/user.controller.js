d801c879aa9a796b7880a83d9fdb422b7a9e8d5b
rajkunwar4 <40rajkunwar@gmail.com>
2024-06-30 04:04:18 +0530

    user routes

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,21 @@
+
+
+export const getAllUsers= async (req,res)=>{
+    res.send("all users");
+}
+export const getUser=async(req,res)=>{
+    res.send("single user")
+}
+
+export const updateUser=async(req,res)=>{
+    res.send("upadate")
+}
+export const deleteUser=async(req,res)=>{
+    res.send("delete")
+}
+
+
+
+
+
+
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import { deleteUser, getAllUsers, getUser, updateUser } from '../controllers/user.controller.js';
+
+const router= express.Router();
+
+router.get("/hello",getAllUsers)
+router.get("/:id",getUser);
+router.put("/:id",updateUser)
+router.delete("/:id",deleteUser)
 
 
-const router=express.Router();
-
-router.get('/test',(req,res)=>{
-    console.log("router works for post");
-    
-;})
 
 export default router;
